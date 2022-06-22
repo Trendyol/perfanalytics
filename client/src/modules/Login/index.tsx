d935483f0337f041d89071a4f738bbc8bb6c419c
Deniz Akpınar <deniz.akpinar@trendyol.com>
2022-06-22 17:07:21 +0300

    refactor(client): add translations for login page

--- client/src/modules/Login/index.tsx
+++ client/src/modules/Login/index.tsx
@@ -1,13 +1,18 @@
 import Button from "@components/shared/Form/Button";
 import Checkbox from "@components/shared/Form/Checkbox";
 import TextField from "@components/shared/Form/TextField";
+import useTranslation from "next-translate/useTranslation";
 import Link from "next/link";
 
 const Login: React.FC = () => {
+  const { t } = useTranslation("login");
+
   return (
     <main className="flex flex-row h-screen overflow-hidden bg-center bg-cover">
       <div className="flex w-1/2 bg-primary lg:hidden overflow-hidden h-full items-center justify-center">
-        <span className="text-white text-5xl font-extrabold">Trendyol</span>
+        <span className="text-white text-5xl font-extrabold">
+          {t("trendyol")}
+        </span>
       </div>
       <div className="flex w-1/2 lg:w-full h-screen overflow-hidden items-center justify-center p-6">
         <div
@@ -16,38 +21,39 @@ const Login: React.FC = () => {
         >
           <div id="header">
             <h1 className="text-5xl sm:text-3xl mb-4 sm:mb-2 text-center">
-              Hello Again!
+              {t("hello_again")}
             </h1>
             <p className="text-sm sm:text-xs text-gray-500 text-center px-8 sm:px-1">
-              Welcome to perfanalytics dashboard. 
+              {t("welcome_to_dashboard")}
             </p>
             <p className="text-sm sm:text-xs text-gray-500 text-center px-8 sm:px-1">
-              Please login to continue.
+              {t("please_login")}
             </p>
           </div>
-
           <div id="content" className="flex flex-col">
-            <TextField type="email" placeholder="Email" />
-            <TextField type="password" placeholder="Password" />
+            <TextField type="email" placeholder={t("email")} />
+            <TextField type="password" placeholder={t("password")} />
             <div className="flex justify-between sm:text-sm">
               <span className="flex items-center">
                 <Checkbox />
-                <button className="ml-1 text-gray-500">Remember me</button>
+                <button className="ml-1 text-gray-500 text-sm">
+                  {t("remember_me")}
+                </button>
               </span>
               <Link href="/">
-                <span className="text-primary cursor-pointer">
-                  Recover Password
+                <span className="text-primary cursor-pointer text-sm">
+                  {t("recover_password")}
                 </span>
               </Link>
             </div>
           </div>
           <div id="actions">
-            <Button>Login</Button>
+            <Button>{t("login")}</Button>
           </div>
         </div>
         <div className="fixed bottom-4 w-full flex justify-center items-center">
-          <span className="text-gray-500">Dont have an account yet?</span>
-          <button className="ml-1 text-primary">Sign Up</button>
+          <span className="text-gray-500">{t("dont_have_account")}</span>
+          <button className="ml-1 text-primary">{t("sign_up")}</button>
         </div>
       </div>
     </main>
