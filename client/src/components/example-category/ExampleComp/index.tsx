53648152ef9daad7c25f449e3d980b0a93abd35d
Deniz Akpınar <ddenizakpinar@gmail.com>
2022-06-30 21:49:10 +0300

    refactor(client): remove unused example folders

--- client/src/components/example-category/ExampleComp/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface ExampleCompProps {}
-
-const ExampleComp = (props: ExampleCompProps) => {
-  return <div data-testid="example-comp">ExampleComp</div>;
-};
-
-export default ExampleComp;
