854f364e5448315f5d6e0f69a441efcb8c7c204b
gizayeroglu <gizay.eroglu@trendyol.com>
2022-09-02 12:27:40 +0300

    feat(backend) fix mail to send user

--- backend/src/modules/user/helpers/mailHelper.ts
+++ backend/src/modules/user/helpers/mailHelper.ts
@@ -4,11 +4,11 @@ export const renderMailContent = (name, url, language = defaultLanguage) => {
   return `<!DOCTYPE html>
 		<html>
 		<head>
-				<title>${mailinator.mail.name[language]}</title>
+				<title>${mailinator.mail.header[language]}</title>
 		</head>
 		<body>
 				<div>
-						<h3>${mailinator.mail.header[language]} ${name},</h3>
+						<h3>${mailinator.mail.name[language]} ${name},</h3>
 						<p>${mailinator.mail.paragraphPart1[language]} <a href=${url}>${mailinator.mail.paragraphPart2[language]}</a> ${mailinator.mail.paragraphPart3[language]}</p>
 						<br>
 						<p>${mailinator.mail.footer[language]}</p>
