d2a7c4f548b379ea8babf2ab74e2e129ead5591d
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-01-22 16:03:01 +0300

    refactor: remove lighthouse module

--- backend/src/modules/lighthouse/constants.ts
+++
@@ -1,9 +0,0 @@
-export const usefulAudits = [
-  'first-contentful-paint',
-  'speed-index',
-  'largest-contentful-paint',
-  'first-meaningful-paint',
-  'total-blocking-time',
-  'cumulative-layout-shift',
-  'interactive',
-];
