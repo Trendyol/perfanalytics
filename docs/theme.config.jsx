88dd1151a0466caf28f96bce11a4023c92b67393
Deniz Akpınar <ddenizakpinar@gmail.com>
2023-01-24 22:23:29 +0300

    feat(docs): create docs

---
+++ docs/theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+  logo: <span>Perfanalytics Docs</span>,
+  project: {
+    link: "https://github.com/Trendyol/perfanalytics",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "Perfanalytics | %s",
+    };
+  },
+  docsRepositoryBase: "https://github.com/Trendyol/perfanalytics",
+};
