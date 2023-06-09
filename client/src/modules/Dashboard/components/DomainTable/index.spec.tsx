8184b8735dd3669e8a21b7bb2ae03a8064f2ef8d
Mert Can Altın <mertgold60@gmail.com>
2023-06-09 11:27:56 +0300

    moved to mock test file

--- client/src/modules/Dashboard/components/DomainTable/index.spec.tsx
+++ client/src/modules/Dashboard/components/DomainTable/index.spec.tsx
@@ -1,6 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import DomainTable from './index';
 
+jest.mock('next/router', () => ({
+  __esModule: true,
+  useRouter: jest.fn().mockReturnValue({
+    pathname: '/',
+    route: '/',
+    query: {},
+    asPath: '/',
+    components: {},
+    isFallback: false,
+    basePath: '',
+    locale: 'en',
+    locales: ['en', 'tr'],
+    defaultLocale: 'en',
+    isReady: true,
+    isPreview: false,
+    isLocaleDomain: false,
+    events: {},
+    push: jest.fn(),
+    replace: jest.fn(),
+    reload: jest.fn(),
+    back: jest.fn(),
+    prefetch: jest.fn(),
+    beforePopState: jest.fn(),
+    isLocaleDomainInternal: jest.fn(),
+    isSsr: true,
+    isReadyInternal: true,
+  }),
+}));
+
 describe('DomainTable', () => {
   it('renders the table with the correct column labels', () => {
     render(<DomainTable />);
