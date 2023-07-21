759991ddf428af1a092f3460efe6cb68434fd2ab
kayraberktuncer <kayrabtuncer@gmail.com>
2023-07-21 11:13:36 +0300

    chore: add test logs for base url

--- client/src/components/layout/Header/index.tsx
+++ client/src/components/layout/Header/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import UserSection from "./components/UserSection";
+import { useConfig } from "@contexts/ConfigContext";
+import axios from "axios";
 
 const Header = () => {
+  const config = useConfig();
+  console.log('baseUrl', config.baseUrl, axios.defaults.baseURL);
+
   return (
     <header className="flex justify-end h-28 w-full">
       <UserSection />
